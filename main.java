4e1be65c512a7e4b8aa86def368f34320b5022b7
J000Zhou <41312297+nathan-boc@users.noreply.github.com>
2023-04-18 21:48:59 +1000

    Inputted node IDs into code

--- main.java
+++ main.java
@@ -1,6 +1,19 @@
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("hi");
+        
+        int bitSize = 9;
+        int numberOfNodes = 10;
+        
+        int[] nodes = {0, 85, 133, 182, 210, 245, 279, 324, 395, 451};
+        
+        
+        for(int i = 0; i < nodes.length; i++) {
+            if(i == nodes.length - 1) {
+                System.out.println(nodes[i]);
+            } else {
+                System.out.print(nodes[i] + ", ");
+            }
+        }
     }
 }
